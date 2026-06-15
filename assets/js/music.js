3bc1417548625fd1c2093295c4c567cde30692d2
Danaria Khoirul Anam <54716014+webprogrammingunida@users.noreply.github.com>
2026-06-15 21:26:39 +0700

    Create Music module with basic structure
    Initialize Music module with play, pause, toggle, and init functions.

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,1 +1,31 @@
+const Music = (() => {
 
+    const audio = document.querySelector('#bgMusic');
+
+    const button = document.querySelector('#musik-btn');
+
+    function play(){
+
+    }
+
+    function pause(){
+
+    }
+
+    function toggle(){
+
+    }
+
+    function init(){
+
+    }
+
+    return {
+
+        init
+
+    };
+
+})();
+
+Music.init();
