eda8ebfed673935c0527cd91ddb0b298f750a166
Danaria Khoirul Anam <54716014+webprogrammingunida@users.noreply.github.com>
2026-06-16 04:26:18 +0700

    Initialize AOS on DOMContentLoaded

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,37 @@ import './navbar.js';
 import './gift.js';
 import './rsvp.js';
 import './admin.js';
+
+/* ---------------------------------- */
+
+if ("scrollRestoration" in history) {
+
+    history.scrollRestoration = "manual";
+
+}
+
+window.scrollTo(0, 0);
+
+/* ---------------------------------- */
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    () => {
+
+        if (typeof AOS !== "undefined") {
+
+            AOS.init({
+
+                duration: 1000,
+
+                once: true
+
+            });
+
+        }
+
+    }
+
+);
